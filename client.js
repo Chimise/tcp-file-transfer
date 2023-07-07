b5c9f45232f34676f4aa470de4c0a71f851f969e
chisom promise <chimisepro@gmail.com>
2023-07-08 00:43:06 +0100

    fix: update a bug

--- client.js
+++ client.js
@@ -1,12 +1,29 @@
 import net from "node:net";
 import multiplexer from "./utils/multiplex.js";
+import {hideBin} from 'yargs/helpers'
+import yargs from "yargs";
+
+const argv = yargs(hideBin(process.argv)).option('ipadrr', {
+  alias: 'ip',
+  description: 'IP address of the recipient server',
+  type: 'string',
+  default: '127.0.0.1'
+}).option('file', {
+  alias: 'f',
+  description: 'Path to the file to be sent',
+  type: 'string',
+  demandOption: true
+}).parse();
 
 async function main() {
-  const serverIp = process.argv[2] || "127.0.0.1";
-  const files = process.argv.slice(3);
+  const serverIp = argv.host;
+  const files = [].concat(argv.file);
+  
+
   if (files.length === 0) {
     throw new Error("At least one file must be provided");
   }
+  
   const client = new net.Socket();
 
   client.connect(3700, serverIp, () => {
